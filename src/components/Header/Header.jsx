a4e642b3a7f902871dcdae6b911ca906bf78ca3d
Konstantin Mack <mack.konstantin@googlemail.com>
2022-08-04 14:19:49 +0100

    header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,9 +5,15 @@ export default function Header() {
   return (
     <header className="header">
       <nav className="header__nav">
-        <NavLink to="/">Stats</NavLink>
-        <NavLink to="/user">Analyser</NavLink>
-        <NavLink to="/tipsters">Tipsters</NavLink>
+        <NavLink to="/" className="header__nav-link">
+          Stats
+        </NavLink>
+        <NavLink to="/user" className="header__nav-link">
+          Analyser
+        </NavLink>
+        <NavLink to="/tipsters" className="header__nav-link">
+          Tipsters
+        </NavLink>
       </nav>
       <button className="header__button">Connect wallet</button>
     </header>

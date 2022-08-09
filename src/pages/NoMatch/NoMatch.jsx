de94d92f5e1ca381e4c0eceb6ce72a5c30d13e9a
Konstantin Mack <mack.konstantin@googlemail.com>
2022-08-09 16:08:12 +0100

    Create NoMatch component

--- src/pages/NoMatch/NoMatch.jsx
+++ src/pages/NoMatch/NoMatch.jsx
@@ -1,5 +1,11 @@
-import React from "react";
+import "./NoMatch.scss";
+import OffsideIcon from "../../assets/icons/offside.svg";
 
 export default function NoMatch() {
-  return <h1>Theres nothing here... 404</h1>;
+  return (
+    <div className="nomatch">
+      <img src={OffsideIcon} alt="offside" className="nomatch__icon" />
+      <h1>Offside!</h1>
+    </div>
+  );
 }

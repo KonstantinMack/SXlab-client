345b194c8bba7c1da2fdc7fa913f94926ce7b47a
Konstantin Mack <mack.konstantin@googlemail.com>
2023-01-13 11:30:13 +0000

    Enable google analytics

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.scss";
 import App from "./App";
+import ReactGA from "react-ga4";
 
 import { BrowserRouter } from "react-router-dom";
+
+ReactGA.initialize("G-7YDXM5J40C");
 
 const queryClient = new QueryClient();
 
